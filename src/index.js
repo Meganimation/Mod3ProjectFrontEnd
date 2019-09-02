1f25a6615eaf5dbcf733ed3fa4b893b4d0dcde25
Megan <MeganOliviaWendyAdams@gmail.com>
2019-09-02 15:39:52 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -15,4 +15,75 @@ addBtn.addEventListener('click', () => {
 })
 
 
-// OR HERE!
+function fetchToys(){
+  fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(renderToys)
+}
+
+
+const toyCollection = document.getElementById('toy-collection')
+function renderToys(toys) {
+  toyCollection.innerHTML = ""
+  toys.forEach(function (toy) {
+    toyCollection.innerHTML += `
+   <div class="card" data-id=${toy.id}>
+        <h2>${toy.name}</h2>
+        <img src="${toy.image}" class="toy-avatar" />
+        <p>${toy.likes} Likes</p>
+        <button class="like-btn">Like <3</button>
+        <button class="delete-btn">Delete</button>
+   </div>
+  `
+  })
+}
+
+fetchToys()
+
+const addToyForm = document.querySelector('.add-toy-form')
+addToyForm.addEventListener('submit', function (event) {
+  fetch('http://localhost:3000/toys/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: `${event.target.name.value}`,
+      image: `${event.target.image.value}`,
+      likes: 0
+    })
+  })
+    .then(resp => resp.json())
+    .then(renderToys)
+})
+
+
+toyCollection.addEventListener('click', function (event) {
+  let likebutton = event.target.className === "like-btn"
+  let delbutton = event.target.className === "delete-btn"
+  if (likebutton) {
+    let id = event.target.parentElement.dataset.id
+    let like = event.target.previousElementSibling
+    let likeCount = parseInt(event.target.previousElementSibling.innerText)
+    like.innerText = `${++likeCount} likes`
+fetch(`http://localhost:3000/toys/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        likes: likeCount
+      })
+    })
+    .then(response => response.json())
+  }
+
+  else if (delbutton) {
+    let id = event.target.parentElement.dataset.id
+    fetch(`http://localhost:3000/toys/${id}`, {
+      method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then(fetchToys)
+  }
+})
